01ed25eecc377b65d080f3aca946d800e08890a2
igonz044 <igonz044@ucr.edu>
2024-08-14 17:35:58 -0700

    add

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Add any interactive functionality here
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message! We will get back to you soon.');
+});
