20e75c9f93e926349f5333004093d488d44364b3
igonz044 <igonz044@ucr.edu>
2024-08-26 18:16:21 -0700

    fixed placement of text

---
+++ sParkit/sParkit/sParkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  sParkitApp.swift
+//  sParkit
+//
+//  Created by Itzel G on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct sParkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
